de99081c97f298a9a133c81baad85d705079b5fe
Jean Santos <jeansantos.sw@gmail.com>
2021-04-01 22:38:20 +0100

    Usando debug do vscode

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
     return res.json({ message: 'Hello world'});
 });
 
+app.post('/courses', (request, response)=>{
+    const { name } = request.body;
+    response.json({ name });
+})
 
-app.listen(3333);
+
+app.listen(3333, ()=>{
+    console.log("Back-and Started!")
+});
