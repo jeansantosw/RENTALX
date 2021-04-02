249b8f82b12b5f3ad5b83cab0191f3f24c2ce69e
Jean Santos <jeansantos.sw@gmail.com>
2021-04-02 13:24:32 +0100

    Routes interface private

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,23 @@
 import { Router } from 'express';
-import { Category } from '../model/Category';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
-const categories: Category[] = [];
+
 
 categoriesRoutes.post('/', (req, res) => {
-
     const { name, description } = req.body;
 
-    const category = new Category();
-    
-    Object.assign(category,{ 
-        name, 
-        description,
-        created_at: new Date()
-     })
+    categoriesRepository.create({name, description});
 
-    categories.push(category);
-    return res.status(201).json({ category });
+    return res.status(201).send();
 });
+
+categoriesRoutes.get('/', (req, res) => {
+    const all = categoriesRepository.list();
+
+    return res.json(all);
+})
 
 export { categoriesRoutes };
