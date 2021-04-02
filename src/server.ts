7cf3f8150dc8c250090c218eb3bd6bcbab10d6a3
Jean Santos <jeansantos.sw@gmail.com>
2021-04-02 11:59:24 +0100

    Criando as rotas

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,10 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    return res.json({ message: 'Hello world'});
-});
-
-app.post('/courses', (request, response)=>{
-    const { name } = request.body;
-    response.json({ name });
-})
-
+app.use(categoriesRoutes);
 
 app.listen(3333, ()=>{
     console.log("Back-and Started!")
