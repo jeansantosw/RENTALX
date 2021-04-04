30b1ddd91487e866237daf2b2515d28e2ee86875
Jean Santos <jeansantos.sw@gmail.com>
2021-04-04 10:37:28 +0100

    Upload de arquivos

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,22 +1,33 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { CategoriesRepository } from '../modules/cars/repositories/implementations/CategoriesRepository';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+    dest: "./tmp"
+});
+
 const categoriesRepository = CategoriesRepository.getInstance();
 
 
 
 categoriesRoutes.post('/', (request, response) => {
-
     return createCategoryController.handle(request, response);
 });
 
 categoriesRoutes.get('/', (request, response) => {
-
     return listCategoriesController.handle(request, response);
+});
+
+categoriesRoutes.post('/import', upload.single("file"), (request, response) => {
+    const { file } = request;
+    console.log(file);
+
+    return response.send()
 
 });
 
