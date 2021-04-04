f9a218c0b2476e649e57e5b38db7d9a694ec0355
Jean Santos <jeansantos.sw@gmail.com>
2021-04-04 12:33:06 +0100

    Pipe, async, biblioteta csv-parse

--- src/modules/cars/useCases/importCategory/ImportCategoriesUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoriesUseCase.ts
@@ -1,8 +1,17 @@
-
+import fs from 'fs';
+import csvParse from "csv-parse";
 
 class ImportCategoriesUseCase{
-    execute(file: any){
-        console.log(file);
+    execute(file: Express.Multer.File): void{
+        const stream = fs.createReadStream(file.path);
+
+        const parseFIle = csvParse();
+
+        stream.pipe(parseFIle);
+
+        parseFIle.on("data", async(line)=>{
+            console.log(line);
+        })
     }
 }
 export { ImportCategoriesUseCase };
