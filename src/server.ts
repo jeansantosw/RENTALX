75ccc8839501082535249db4ed3ab9a473152600
Jean Santos <jeansantos.sw@gmail.com>
2021-04-07 22:32:18 +0100

    ormconfig

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import { router } from './routes';
 import swaggerFile from './swagger.json';
-import "./dataBase";
+//import "./dataBase";
 
 // 
 const app = express();
